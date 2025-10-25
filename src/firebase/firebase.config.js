b0700645d42347c9531766fdcdd1d8e475be0d76
MOHITUL <mohitulislam9678@gmail.com>
2025-10-25 21:34:23 +0600

    added env file

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBTxNaS_FLqn0g6PBX_mYd4W2b0ICRh0Uk",
-  authDomain: "game-keeper-55693.firebaseapp.com",
-  projectId: "game-keeper-55693",
-  storageBucket: "game-keeper-55693.firebasestorage.app",
-  messagingSenderId: "1038485974503",
-  appId: "1:1038485974503:web:746bd8159a67b75db6a142"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
